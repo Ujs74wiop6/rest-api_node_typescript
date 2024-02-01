1ea41fdc5c95824aae763114d87caf63ba1291a6
Fabricio Tiago Arantes <88052209+Ujs74wiop6@users.noreply.github.com>
2024-02-01 16:12:18 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-Server_1.server.listen(process.env.PORT, () => {
-    console.log(`App: ON-LINE! na porta: ${process.env.PORT}`);
-}).on('error', (err) => {
-    console.log('App: OFF-LINE | Erro ao iniciar o servidor: ', err.message);
-});
